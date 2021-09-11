4fbbfd806e95299d5c5abd771ca2574a59901bf3
ashvinsunga <ashvinsunga@yahoo.com.ph>
2021-09-11 21:14:17 +0800

    done

--- script.js
+++ script.js
@@ -6,3 +6,21 @@
 // through the sorted portion -i.e. the left side- to place the element in the correct place.
 // Repeat until the array is sorted.
 
+// Time Complexity - O(n^2)
+function insertionSort(arr){
+    console.log(arr, "input!")
+	var currentVal;
+    for(var i = 1; i < arr.length; i++){
+        currentVal = arr[i];
+        for(var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+            arr[j+1] = arr[j]
+            console.log(arr);
+        }
+        arr[j+1] = currentVal;
+        console.log(arr,"outside!")
+    }
+    return arr;
+}
+
+insertionSort([2,1,9,76,4])
+//                    j i          curVal = 4
